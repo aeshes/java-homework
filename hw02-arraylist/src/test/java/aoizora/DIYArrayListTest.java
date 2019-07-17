f78bdf2c65edd1d040a43cc25dd16c279cd601f7
asmirnov <asmirnov@innoseti.ru>
2019-07-17 13:34:33 +0200

    Unit testing

--- hw02-arraylist/src/test/java/aoizora/DIYArrayListTest.java
+++ hw02-arraylist/src/test/java/aoizora/DIYArrayListTest.java
@@ -1,17 +1,22 @@
 package aoizora;
 
+import com.google.common.collect.Ordering;
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DIYArrayListTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void get() {
         DIYArrayList arrayList = new DIYArrayList();
         arrayList.add("a");
         assertEquals("a", arrayList.get(0));
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void size() {
         DIYArrayList<String> arrayList = new DIYArrayList<>();
 
@@ -20,5 +25,35 @@ class DIYArrayListTest {
 
         arrayList.add("b");
         assertEquals(2, arrayList.size());
+    }
+
+    @Test
+    void testAddAll() {
+        DIYArrayList<Integer> arrayList = new DIYArrayList<>();
+        Collections.addAll(arrayList, 1, 2, 3, 4, 5, 9, 8, 7, 6, 12, 13, 14, 15, 20, 19, 18,  17, 16);
+        assertEquals(arrayList.size(), 18);
+    }
+
+    @Test
+    void testCopy() {
+        DIYArrayList<UUID> arrayList = new DIYArrayList<>();
+        List<UUID> src = new ArrayList<>();
+        for (int i = 0; i < 20; ++i) {
+            src.add(UUID.randomUUID());
+        }
+        Collections.copy(arrayList, src);
+        assertEquals(src.size(), arrayList.size());
+    }
+
+    @Test
+    void testSort() {
+        DIYArrayList<Integer> arrayList = new DIYArrayList<>();
+        Random rand = new Random();
+        for (int i = 0; i < 20; ++i) {
+            int n = rand.nextInt(100);
+            arrayList.add(n);
+        }
+        Collections.sort(arrayList, Comparator.naturalOrder());
+        assertTrue(Ordering.natural().isOrdered(arrayList));
     }
 }
