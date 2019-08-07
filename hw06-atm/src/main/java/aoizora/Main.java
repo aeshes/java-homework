0608fc522815a1bd8416bd0f0ce4a1c969bc1284
aoizora <asmirnov@innoseti.ru>
2019-08-07 10:29:00 +0200

    Тестовый запуск

--- hw06-atm/src/main/java/aoizora/Main.java
+++ hw06-atm/src/main/java/aoizora/Main.java
@@ -1,8 +1,18 @@
 package aoizora;
 
+import aoizora.atm.Atm;
+import aoizora.atm.Banknote;
+
 public class Main {
 
     public static void main(String[] args) {
+        Atm atm = new Atm();
 
+        atm.oneSlot(Banknote.one());
+        atm.fiveSlot(Banknote.five());
+        atm.tenSlot(Banknote.ten());
+
+        atm.withdraw(13)
+                .forEach(banknote -> System.out.println(banknote.getValue() + " banknotes"));
     }
 }
