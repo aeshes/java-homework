e28d8ef4aeb2914a0626abc4dffd4c6a638c46de
aoizora <aoizora@archlinux>
2019-09-20 18:22:39 +0300

    Написал getSessionFactory

--- hw10-hibernate/src/main/java/aoizora/Main.java
+++ hw10-hibernate/src/main/java/aoizora/Main.java
@@ -1,8 +1,16 @@
 package aoizora;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +21,20 @@ public class Main {
     static EntityManager getEntityManager() {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("common-unit");
         return emf.createEntityManager();
+    }
+
+    static SessionFactory getSessionFactory(String configFile, Class... annotatedClasses) {
+        Configuration configuration = new Configuration().configure(configFile);
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        MetadataSources mds = new MetadataSources(registry);
+
+        Arrays.stream(annotatedClasses)
+                .forEach(mds::addAnnotatedClass);
+        Metadata metadata = mds.getMetadataBuilder().build();
+
+        return metadata.getSessionFactoryBuilder().build();
     }
 }
