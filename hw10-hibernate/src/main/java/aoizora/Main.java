53d20a20cac94d94c2ae3d5090a418e39ab07957
aoizora <aoizora@archlinux>
2019-09-20 17:57:50 +0300

    Создал EntityManager

--- hw10-hibernate/src/main/java/aoizora/Main.java
+++ hw10-hibernate/src/main/java/aoizora/Main.java
@@ -1,7 +1,17 @@
 package aoizora;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
     public static void main(String[] args) {
+        EntityManager em = getEntityManager();
+        System.out.println(em);
+    }
 
+    static EntityManager getEntityManager() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("common-unit");
+        return emf.createEntityManager();
     }
 }
